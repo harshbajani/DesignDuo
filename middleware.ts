0c03ae023e0934419377c55079a8935c207320ad
Harshbajani <47421975+harshbajani@users.noreply.github.com>
2024-03-09 14:56:18 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
-  // ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/transformations/add/restore"]
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)"]
 });
  
 export const config = {
